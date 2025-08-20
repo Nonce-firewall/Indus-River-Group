c781c4790ca1c3d8fafc38668d4c6196180ef968
G-Gauravlal <g.gauravlal@gmail.com>
2025-08-21 02:59:04 +0530

    Remove experimental configurations causing Next.js AsyncLocalStorage error

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable experimental features that might cause memory issues
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  // Disable SWC completely
-  swcMinify: false,
-  // Reduce memory usage
-  webpack: (config, { dev, isServer }) => {
-    // Reduce memory usage
-    config.optimization = {
-      ...config.optimization,
-      minimize: false, // Disable minification entirely for now
-    };
-    
-    // Limit parallel processing
-    config.parallelism = 1;
-    
-    return config;
-  },
-  // Static export settings
   output: 'standalone',
   trailingSlash: true,
   eslint: {
@@ -33,5 +13,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 };
+
 
 module.exports = nextConfig;
