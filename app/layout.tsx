2df407bba932a1c4bf12b1d7364e9f7d40fb1c96
G-Gauravlal <g.gauravlal@gmail.com>
2025-08-29 08:03:01 +0530

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
   keywords: [
     'private equity', 'small business acquisition', 'patient capital', 'business stewardship',
     'investment firm', 'independent sponsor', 'business partnership', 'operational excellence',
+  ]
   themeColor: '#08164F',
   authors: [{ name: 'Indus River Group' }],
   creator: 'Indus River Group',
